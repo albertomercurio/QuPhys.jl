a16edcc197623cdcbefed497ba9ceaa323755661
Documenter.jl <documenter@juliadocs.github.io>
2022-03-15 14:33:06 +0000

    build based on 3a363b9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QuPhys","category":"page"},{"location":"#QuPhys","page":"Home","title":"QuPhys","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuPhys.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QuPhys]","category":"page"}]
+}
